bd76b21624d5dcf12ca4ce798f465535b7db211d
Saurabh <saurabh@qatechhub.com>
2020-09-25 08:04:06 +0530

    Added POM.xml

---
+++ ModularFramework-25092020/src/main/java/commonLibs/Implementation/CommonDriver.java
@@ -0,0 +1,11 @@
+package commonLibs.Implementation;
+
+import org.openqa.selenium.WebElement;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class CommonDriver {
+	
+	private AndroidDriver<WebElement> driver;
+
+}
